311c39b0818f923329986b47fb775461840d23c3
Patryk Kubiczek <patryk.kubiczek@instrat.pl>
2023-02-16 16:52:21 +0100

    Tailored calculation of reduction targets for Grupa Kęty, Enea and Grupa Azoty

--- scripts/process_targets.py
+++ scripts/process_targets.py
@@ -8,9 +8,28 @@ def process_targets(suffix=""):
     input_file = data_dir("clean", f"input_data{suffix}.xlsx")
     df = pd.read_excel(input_file, sheet_name="target_data")
 
-    df = df[df["target_type"] == "Absolute"]
+    # Allow Grupa Kęty's intensity target
+    df = df[(df["target_type"] == "Absolute") | (df["company_name"] == "Grupa Kęty")]
 
-    # TODO: aggregate Grupa Azoty targets
+    # Since Enea has small S2 emissions, assume S1 target is S1+S2 target
+    df.loc[(df["company_name"] == "Enea") & (df["scope"] == "S1"), "scope"] = "S1+S2"
+
+    # Aggregate Grupa Azoty S1+S2 target
+    df_azoty = df[df["company_name"] == "Grupa Azoty"].copy()
+
+    for scope in ["S1", "S2"]:
+        is_scope = df_azoty["scope"] == scope
+        df_azoty.loc[is_scope, "reduction_ambition"] *= df_azoty.loc[
+            is_scope, f"base_year_ghg_{scope.lower()}"
+        ] / df_azoty.loc[is_scope, ["base_year_ghg_s1", "base_year_ghg_s2"]].sum(axis=1)
+    df_azoty = (
+        df_azoty.groupby(["company_name", "base_year", "end_year"])
+        .agg({"reduction_ambition": "sum"})
+        .reset_index()
+    )
+    df_azoty["scope"] = "S1+S2"
+    df = pd.concat([df, df_azoty])
+
     df = df[df["scope"].isin(["S1+S2", "S1+S2+S3"])]
 
     df = df[["company_name", "reduction_ambition", "base_year", "end_year"]]
